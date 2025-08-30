8eabd656b2264c5733d3736c986b27f37df5c0ee
Eliseo Orozco <eliseorozcow@gmail.com>
2025-08-29 22:19:01 -0300

    Add TaskService and update TaskController
    Introduced TaskService to handle task-related operations and updated TaskController to use TaskService for retrieving tasks. Removed the unused TodoServices class.

--- SpringToDoApp/src/main/java/controller/TaskController.java
+++ SpringToDoApp/src/main/java/controller/TaskController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import models.Task;
+import org.springframework.stereotype.Controller;
+import services.TaskService;
+
+import java.util.List;
+
+@Controller
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    public String getTasks(){
+        List<Task> tasks = taskService.getAllTasks();
+        return "tasks";
+    }
 }
---
+++ SpringToDoApp/src/main/java/services/TaskService.java
@@ -0,0 +1,13 @@
+package services;
+
+import models.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    public List<Task> getAllTasks() {
+    }
+}
