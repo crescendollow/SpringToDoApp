abeb770b33b21fb580225b39da7ea3b9b00bfd63
Eliseo Orozco <eliseorozcow@gmail.com>
2025-09-01 22:42:57 -0300

    Add task list rendering to tasks.html
    Updated tasks.html to display a list of tasks using Thymeleaf. Also added import for @RequestMapping in TaskController for future route management.

--- SpringToDoApp/src/main/java/controller/TaskController.java
+++ SpringToDoApp/src/main/java/controller/TaskController.java
@@ -5,11 +5,13 @@ import models.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import services.TaskService;
 
 import java.util.List;
 
 @Controller
+//@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
